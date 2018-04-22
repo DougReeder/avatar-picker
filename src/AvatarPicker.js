28f52b7276e3182edc45a3d7f9289d33afb05a7c
P. Douglas Reeder <reeder.29@gmail.com>
2018-04-22 16:56:21 -0400

    Selecting avatar trigger fake fetch, then selects it

--- src/AvatarPicker.js
+++ src/AvatarPicker.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Avatar from './Avatar';
 
+function fakeFetch(url) {
+    return new Promise(function(resolve, reject) {
+        let response = new Response("foo", {status: 200, statusText: "OK"});
+        setTimeout(resolve, 1000, response);
+    });
+}
+
 class AvatarPicker extends Component {
     constructor(props) {
         super(props);
@@ -11,21 +18,33 @@ class AvatarPicker extends Component {
         // console.log("constructing AvatarPicker", props, this.state);
     }
 
+    findAvatarById(avatarId) {
+        return this.props.avatars.find( (avatar) => {
+            return avatar.id === avatarId;
+        });
+    }
+
     clickCurrent() {
         console.log("clickCurrent", !this.state.pickerDisplayed);
         this.setState({pickerDisplayed: !this.state.pickerDisplayed})
     }
 
+    clickSelect(avatarId) {
+        console.log("clickSelect", avatarId);
+        fakeFetch("https://example.com/api/foo").then((response) => {
+            // console.log("fetch response:", response);
+            this.setState({currentId: avatarId, pickerDisplayed: false})
+        })
+    }
+
     render() {
-        let currentAvatar = this.props.avatars.find( (avatar) => {
-            return avatar.id === this.state.currentId;
-        });
+        let currentAvatar = this.findAvatarById(this.state.currentId);
         // console.log("currentAvatar:", currentAvatar);
 
         let picker;
         if (this.state.pickerDisplayed) {
             let avatars = this.props.avatars.map((avatar) => {
-                return <Avatar key={avatar.src} pic={avatar.src}/>;
+                return <Avatar key={avatar.src} pic={avatar.src} onClick={() => this.clickSelect(avatar.id)}/>;
             });
             picker = (
                 <div className="picker">
